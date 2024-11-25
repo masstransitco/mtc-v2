c12e9f89ee0525b6ded2f23e0aea850f5f8d2f3a
Neuter <mark@air.city>
2024-11-25 09:45:24 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,38 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: './src/index.js',
-    fleet: './src/fleet-management.js'
+    main: './src/index.js',     // Entry point for Sign-in & Verification
+    fleet: './src/fleet.js'     // Entry point for Fleet Management
   },
   output: {
-    filename: '[name].bundle.js', // Generates main.bundle.js and fleet.bundle.js
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/',
+    filename: '[name].bundle.js',   // Generates main.bundle.js and fleet.bundle.js
+    path: path.resolve(__dirname, 'public'), // Output directory
+    publicPath: '/',                 // Public URL of the output directory when referenced in a browser
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Transpile .js files
-        exclude: /node_modules/,
+        test: /\.js$/,               // Apply this rule to .js files
+        exclude: /node_modules/,     // Exclude dependencies in node_modules
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader',    // Use Babel loader to transpile JavaScript
           options: {
-            presets: ['@babel/preset-env'], // Use preset-env for modern JavaScript
+            presets: ['@babel/preset-env'], // Preset for compiling ES6+ syntax
           },
         },
       },
+      {
+        test: /\.css$/,              // Apply this rule to .css files
+        use: ['style-loader', 'css-loader'], // Enables importing CSS into JS
+      }
     ],
   },
   devServer: {
-    static: path.join(__dirname, 'public'),
-    compress: true,
-    port: 9000,
+    static: path.join(__dirname, 'public'), // Serve static files from 'public' directory
+    compress: true,                          // Enable gzip compression
+    port: 9000,                              // Port to run the dev server
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: ['.js'],                     // Resolve these extensions
   },
 };
