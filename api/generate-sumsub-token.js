cd1f675df06cd663c37c2f3e4451d743a393a1f6
Neuter <mark@air.city>
2024-11-25 14:20:38 +0800

    Update generate-sumsub-token.js

--- api/generate-sumsub-token.js
+++ api/generate-sumsub-token.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 
 const cors = Cors({
   allowMethods: ['POST', 'OPTIONS'],
-  origin: 'https://www.masstransit.company', // Replace with your actual frontend domain
+  origin: 'hhttps://mtc-v2.vercel.app/', // Replace with your actual frontend domain
 });
 
 const handler = async (req, res) => {
